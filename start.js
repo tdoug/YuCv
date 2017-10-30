41337aba558ba3af6a5884f15decef14d2f0674d
Tom Douglas <tomdoug@gmail.com>
2017-10-30 00:09:16 -0500

    Basic Auth and Cleanup

--- start.js
+++ start.js
@@ -1,7 +1,16 @@
 var path = require('path')
 var fs = require('fs')
 
-var app = require('express')()
+var express = require('express')
+var basicAuth = require('basic-auth-connect');
+
+var app = express()
+
+// adding a check to lock the app with a u/p in case I decide to deploy this
+if (fs.existsSync(__dirname + '/.lock-server')) {
+  app.use(basicAuth('user', 'pass'))
+}
+
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
 
@@ -14,9 +23,9 @@ var youtubedl = require('youtube-dl')
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.post('/convert', function(req, res) {
-    var rawUrl = req.body.url
-    var parser = queryString.parse(rawUrl.replace(/^.*\?/, ''))
-    var id = parser.v
+    var rawUrl = req.body.url // url passed from form
+    var parser = queryString.parse(rawUrl.replace(/^.*\?/, '')) // parse query string of url
+    var id = parser.v // find unique ID of YouTube video
     var vidPath = '/tmp/'+id+'.mp4'
     var mp3Path = '/tmp/'+id+'.mp3'
     var title, size
@@ -25,6 +34,7 @@ app.post('/convert', function(req, res) {
     var cvProgress = 0
     var cPct = 0
 
+    // start download
     var video = youtubedl(rawUrl)
     video.on('info', function(info) {
 	  title = info.title
@@ -35,40 +45,47 @@ app.post('/convert', function(req, res) {
       dlProgress += chunk.length;
       dlPct = dlProgress / size * 100
       dlPct = Math.floor(dlPct)
+      // send a socket event with the current download progress
       io.emit('download', { pct: dlPct });
 	})
 
     video.pipe(fs.createWriteStream(vidPath))
 
+    // download has finished, begin conversion
     video.on('end', function() {
+
 	    ffmpeg(vidPath)
 		    .noVideo()
 		    .toFormat('mp3')
 		    .on('progress', function(progress) {
+		       // send a socket event  with the timemark of current conversion
 			   io.emit('convert', { timemark: progress.timemark });
 			 })
 		    .on('end', function() {
 			    fs.readFile(mp3Path, function(err, data) {
+			    	// set HTTP headers and send file to the browser for download
 			    	res.setHeader('Content-Length', fs.statSync(mp3Path).size);
 					res.setHeader('Content-Type', 'audio/mpeg');
 					res.setHeader('Content-Disposition', 'attachment; filename='+title+'.mp3');
 					res.write(data, 'binary');
+					// delete temporary files
 					fs.unlink(vidPath, function (){})
 			    	fs.unlink(mp3Path, function (){})
 					res.end();
 			    })
 			 })
 		    .save(mp3Path)
+		    
 	})
 });
 
+app.use(express.static('public'))
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/public/go.html'));
 });
 
-io.on('connection', function(){  
-  console.log('socket connected')
-});
+// initialize socket
+io.on('connection', function(){})
 
 http.listen(3000, function() { console.log('listening')});
